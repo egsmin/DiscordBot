21174326580cdd49c1e88bbbfa4baecae811ba04
emreymrt <emre@yumurtaci.de>
2022-12-21 17:56:15 +0100

    Setting Up Project Structure

---
+++ Library/Bundles.py
@@ -0,0 +1,23 @@
+class StartMenuBundle:
+    pass
+
+class ConfBundle:
+    pass
+
+class IntroductionBundle:
+    pass
+
+class TutorialBundle:
+    pass
+
+class PreBundle:
+    pass
+
+class LearnBundle:
+    pass
+
+class PauseBundle:
+    pass
+
+class EndBundle:
+    pass
